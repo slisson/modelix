bc80b1efd38fe5fd0a0d8ad1f418be6955305b82
Sascha Lisson <sl@slisson.de>
2021-06-01 07:51:34 +0200

    attemtp to fix the "peer not authenticated" error on travis (4)

--- certificates/add-to-keystore.sh
+++ certificates/add-to-keystore.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cp $parent_path/itemis.de.cer $JAVA_HOME/lib/security/
+cp certificates/itemis.de.cer $JAVA_HOME/lib/security/
 cd $JAVA_HOME/lib/security/
 keytool -import -alias itemis.de -file itemis.de.cer -keystore cacerts -storepass changeit -noprompt
