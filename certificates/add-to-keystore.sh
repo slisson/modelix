3597cbc38cb1453cd2bc3db59a85b0c7e9083a8d
Sascha Lisson <sl@slisson.de>
2021-06-01 07:54:37 +0200

    attempt to fix the "peer not authenticated" error on travis (4)

--- certificates/add-to-keystore.sh
+++ certificates/add-to-keystore.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ls $JAVA_HOME
 cp certificates/itemis.de.cer $JAVA_HOME/lib/security/
 cd $JAVA_HOME/lib/security/
 keytool -import -alias itemis.de -file itemis.de.cer -keystore cacerts -storepass changeit -noprompt
